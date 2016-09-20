4734a736ecd27dfac079870689c40cc8c5bd3247
ylsGit <bzwdyls@163.com>
2016-09-20 11:14:33 +0800

    修改

--- chaincode/fund_management.go
+++ chaincode/fund_management.go
@@ -473,15 +473,15 @@ func (t *FundManagementChaincode) transferFund(stub shim.ChaincodeStubInterface,
 		return nil, err
 	}
 
-	owner, err := stub.GetCallerCertificate()
+	_, err = stub.GetCallerCertificate()
 	if err != nil {
 		return nil, fmt.Errorf("Get caller certificate failed:%s", err)
 	}
 
-	_, _, err = getUserInfo(stub, fundName, owner)
-	if err != nil {
-		return nil, err
-	}
+	// _, _, err = getUserInfo(stub, fundName, owner)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	//验证限制是否满足
 
